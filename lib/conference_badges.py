892e258152a80d201ca8147ae61c5b3235dde2f5
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-09 15:25:26 -0500

    Badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,38 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    
+    l = []
+
+    for name in names:
+        l.append(f"Hello, my name is {name}.")
+
+    return l
+
+# batch_badge_creator(names)
 
 def assign_rooms(names):
-    return None
+    
+    l = []
+    num = 1
+
+    for name in names:
+        l.append(f"Hello, {name}! You'll be assigned to room {num}!")
+        num += 1
+    
+    return l
+
+# assign_rooms(names)
+# names = ["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def printer(names):
-    return None
+    
+    for badge in batch_badge_creator(names):
+        print(badge)
+    
+    for assignment in assign_rooms(names):
+        print(assignment)
+
+
+# printer(names)
